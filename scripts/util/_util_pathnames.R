5a81ac2e9ddc2b3e0e30c77d7a282baed68ccae7
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2020-07-28 13:24:47 -0400

    Update trajectories code

--- scripts/util/_util_pathnames.R
+++ scripts/util/_util_pathnames.R
@@ -4,8 +4,9 @@
 # 
 # ---------------------------------------------------------------
 
-p_proj <- "/Users/christophercrawford/Google Drive/_Projects/abandonment_trajectories"
-p_scripts <- paste0(p_proj, "/scripts")
-p_dat <- "/Users/christophercrawford/Google Drive/_Projects/data"
-p_output <- paste0(p_proj, "/output")
+p_proj <- "/Users/christophercrawford/Google Drive/_Projects/abandonment_trajectories/"
+p_scripts <- paste0(p_proj, "scripts/")
+p_dat <- "/Users/christophercrawford/Google Drive/_Projects/data/"
+p_dat_derived <- paste0(p_proj, "data_derived/")
+p_output <- paste0(p_proj, "output/")
 
