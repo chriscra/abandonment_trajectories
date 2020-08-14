76111788dae976f923d41fd0d8bdf303adc687ac
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2020-08-13 23:18:49 -0400

    Update file name and delete _util_dt_functions.R

--- scripts/util/_util_master.R
+++ scripts/util/_util_master.R
@@ -6,4 +6,6 @@
 source("scripts/util/_util_misc_functions.R")
 source("scripts/util/_util_spatial_functions.R")
 
+source("scripts/util/_util_dt_filter_functions.R")
+
 source("scripts/util/_util_pathnames.R")
