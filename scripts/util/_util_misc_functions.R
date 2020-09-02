da9fa284f05e418c3b3dc68654d72940788a4f42
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2020-09-01 21:38:44 -0400

    Update gif function

--- scripts/util/_util_misc_functions.R
+++ scripts/util/_util_misc_functions.R
@@ -60,3 +60,20 @@ cc_create_bin <- function(x) {
   dt
 }
 
+
+# ------------------------- #
+# capitalize name
+# ------------------------- #
+capwords <- function(s, strict = FALSE) {
+  cap <- function(s) {
+    paste(
+      toupper(substring(s, 1, 1)), 
+      {s <- substring(s, 2); if(strict) tolower(s) else s},
+      sep = "", collapse = " "
+    )
+  }
+  sapply(strsplit(s, split = " "), 
+         cap, 
+         USE.NAMES = !is.null(names(s))
+  )
+}
