89b46f78fccab97272e93116a9f89dae0b3aefdc
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2020-09-15 21:59:14 -0400

    Update raster names

--- scripts/util/_util_pathnames.R
+++ scripts/util/_util_pathnames.R
@@ -6,7 +6,9 @@
 
 p_proj <- "/Users/christophercrawford/Google Drive/_Projects/abandonment_trajectories/"
 p_scripts <- paste0(p_proj, "scripts/")
+
 p_dat <- "/Users/christophercrawford/Google Drive/_Projects/data/"
+p_bel_tiles <- "/Users/christophercrawford/Google Drive/_Projects/data/Abandonment/belarus_tiles/"
+
 p_dat_derived <- paste0(p_proj, "data_derived/")
 p_output <- paste0(p_proj, "output/")
-
