f63701e208c0d874f00f062b7ba2937d8601ebac
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2021-02-05 14:09:45 -0500

    Update script to accept SLURM array arguments

--- scripts/cluster/dt_max_age_to_raster.R
+++ scripts/cluster/dt_max_age_to_raster.R
@@ -5,6 +5,34 @@
 # -------------------------------------------------------- #
 # Note: as of February 2nd, 2021, this has yet to be run. I updated the input path, so should work with how things are structured. 
 
+# array set up
+args <- commandArgs(TRUE) # access the slurm array variable
+indx <- as.numeric(args[1])
+
+# set up parameters:
+# data.frame of all sites contains information about sites
+site_df <- read.csv(file = paste0(p_dat_derived, "site_df.csv"))
+
+# site_list <- c("belarus", "bosnia_herzegovina", "chongqing", 
+#                "goias", "iraq", "mato_grosso", 
+#                "nebraska", "orenburg", "shaanxi", 
+#                "volgograd", "wisconsin")
+# 
+# site_label_list <- c("_b", "_bh", "_c", 
+#                      "_g", "_i", "_mg", 
+#                      "_n", "_o", "_s", 
+#                      "_v", "_w")
+
+site <- site_df$site[indx] # set site:
+site_label <- site_df$label[indx] # set label
+
+blip_label <- "_b1"
+label <- NULL # for calculating the max age
+
+cat("Set up site parameters:\n")
+print(t(site_df[indx, ]))
+
+
 # load libraries
 cluster_packages <- c("data.table", "tictoc")
 install_pkg <- lapply(cluster_packages, library, character.only = TRUE)
@@ -13,9 +41,6 @@ install_pkg <- lapply(cluster_packages, library, character.only = TRUE)
 # file_in <- "/scratch/network/clc6/abandonment_trajectories/data/belarus.tif"
 p_dat_derived <- "/scratch/network/clc6/abandonment_trajectories/data_derived/"
 p_input <- "/scratch/network/clc6/abandonment_trajectories/data_derived/input_rasters"
-
-# set site:
-site <- "belarus"
 
 
 tic.clearlog()
@@ -33,8 +58,9 @@ toc()
 
 # write raster
 tic("write raster")
-writeRaster(r, filename = paste0(p_input, site, "_max_length.tif"))
+writeRaster(r, filename = paste0(p_input, site, "_max_length", blip_label, ".tif"))
 toc()
+
 names(r)
 print(r)
 
