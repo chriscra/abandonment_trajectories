a9e5ee2fe76f26873bc89a2a0f4094e6f20f749f
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2021-04-12 17:04:44 -0400

    Testing script to make link to google earth engine

--- scripts/python/ee_start.py
+++ scripts/python/ee_start.py
@@ -6,5 +6,18 @@ Created on Thu Jul  9 16:17:45 2020
 @author: christophercrawford
 """
 import ee
+# ee.Authenticate()
+
+ee.Initialize()
 
 
+test = 'does it work?'
+print(test)
+
+
+# define my small region for testing:
+shaanxi = ee.Image('users/chriscra/abandonment/input_rasters/shaanxi')
+
+
+
+print('shaanxi')
