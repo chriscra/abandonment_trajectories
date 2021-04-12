b2798bc7fdae1dcef89fadabc0d374ceaa60c761
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2021-04-12 16:54:24 -0400

    Create script to analyze progression of land cover during regeneration

---
+++ scripts/cluster/4_lc_of_abn.R
@@ -0,0 +1,16 @@
+# Final land cover classes of abandoned and recultivated lands
+# Christopher Crawford, March 23rd, 2021
+# ----------------------------------------------------------------------- #
+
+
+# What proportion of the land that was abandoned at each site falls into the different land cover classes?
+# 
+
+# Take the abandonment age dt, and run the filter again to remove all 0s and all 1s, after the moving window filters.
+# Take the raw land cover dt, and select the matching x and y, and Select a new  this as 1
+
+
+
+# Part II: what land cover class is most likely to be found in newly abandoned land?
+
+# what land cover classes are more likely to get recultivated? What is the LC the year prior to recultivation? 
