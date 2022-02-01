9ad783e8b212c07e917ef94b0a09458a43920947
Christopher Crawford <32919420+chriscra@users.noreply.github.com>
2022-02-01 16:51:53 -0500

    Update to include land.cover temporal filter, new merge function, layer names
    Revisions:
    - Update to include temporal filter step before crop-noncrop binary recoding
    - add new terra function: cc_merge_rasters_terra()
    - add code to save rasters with layer names

--- scripts/cluster/1_prep_r_to_dt.R
+++ scripts/cluster/1_prep_r_to_dt.R
@@ -1,27 +1,27 @@
 # -------------------------------------------------------- #
-# Christopher Crawford, Princeton University, March 4th, 2021
+# Christopher Crawford, Princeton University, March 4th, 2021 (updated January 28th, 2022)
 
 # Script to prep land cover rasters for all sites, 
 # producing data.tables for further analysis.
 # -------------------------------------------------------- #
 # Note: this script merges, recodes, and preps land cover rasters from Yin et al. 2020,
-# saving them for use in "2_analyze_abn.R"
+# saving them for use in "2_analyze_abn.R", "2.2_calc_recult_age.R", and "3_distill_lengths.R"
 
-# It contains three steps:
+# It contains the following steps:
 # 1. cc_merge_rasters() // Merge raw raster layers
 # 2. cc_r_to_dt() // Convert raw rasters into data.tables (including renaming and recoding)
-# 3. save prepped land cover data.tables as rasters.
-
-# SLURM Scripts that accompany this are:
-# analyze_all_sites.slurm, which runs for all sites except belarus (1), which requires a greater memory allocation.
-# orenburg was close, but I was able to make it work with 150 gb. total.
-
+# 3. Pass temporal filter across raw land cover data.tables
+# 4. Save prepped land cover data.tables as rasters.
 
 # -------------------------------------------------------- #
 # load libraries
-cluster_packages <- c("data.table", "tictoc", "raster", "terra", 
+cluster_packages <- c("data.table", "tictoc", 
+                      "raster", "terra", 
                       "landscapemetrics", "landscapetools", "sp",
-                      "tidyverse", "rgdal", "dtraster")
+                      "rgdal", "sf", "fasterize",
+                      "dtraster",
+                      "tidyverse"
+                      )
 install_pkg <- lapply(cluster_packages, library, character.only = TRUE)
 
 # set paths:
@@ -29,7 +29,12 @@ p_dat_derived   <-    "/scratch/gpfs/clc6/abandonment_trajectories/data_derived/
 p_input_rasters <-    "/scratch/gpfs/clc6/abandonment_trajectories/data_derived/input_rasters/"
 p_output        <-    "/scratch/gpfs/clc6/abandonment_trajectories/output/"
 p_raw_rasters   <-    "/scratch/gpfs/clc6/abandonment_trajectories/raw_rasters/"
+p_tmp           <-    "/scratch/gpfs/clc6/abandonment_trajectories/data_derived/tmp/"
 
+
+# set terra autosave options:
+terraOptions(tempdir = p_tmp)
+rasterOptions(tmpdir = p_tmp)
 
 # source functions:
 source("/home/clc6/abandonment_trajectories/scripts/util/_util_functions.R")
@@ -38,6 +43,8 @@ source("/home/clc6/abandonment_trajectories/scripts/util/_util_functions.R")
 # array set up -------
 args <- commandArgs(TRUE) # access the slurm array variable
 indx <- as.numeric(args[1])
+# site_index <- as.numeric(args[1])
+# indx <- 9
 
 
 tic.clearlog()
@@ -46,17 +53,6 @@ tic("full script")
 # set up parameters:
 # data.frame of all sites contains information about sites
 site_df <- read.csv(file = paste0(p_dat_derived, "site_df.csv"))
-
-# site_list <- c("belarus", "bosnia_herzegovina", "chongqing", 
-#                "goias", "iraq", "mato_grosso", 
-#                "nebraska", "orenburg", "shaanxi", 
-#                "volgograd", "wisconsin")
-# 
-# site_label_list <- c("_b", "_bh", "_c", 
-#                      "_g", "_i", "_mg", 
-#                      "_n", "_o", "_s", 
-#                      "_v", "_w")
-
 site <- site_df$site[indx] # set site:
 site_label <- site_df$label[indx] # set label
 
@@ -69,14 +65,13 @@ print(t(site_df[indx, ]))
 # 1. Merge raw raster layers ----
 cat("1. Merge raw raster layers", fill = TRUE)
 tic("Merge rasters")
-cc_merge_rasters(site = site, 
+cc_merge_rasters_terra(site = site, 
                  site_df = site_df, 
                  input_path = p_raw_rasters
                  # ^^ this folder must contain folders with site names, 
                  # with either bricks or multiple raster layer files inside
                  )
 toc(log = TRUE)
-
 
 # -------------------------------------------------------- #
 # 2. Convert raw rasters into data.tables (including renaming and recoding) ----
@@ -90,29 +85,92 @@ toc(log = TRUE)
 
 
 # -------------------------------------------------------- #
-# 3. (Recently added). Save renamed and recoded input land cover data.tables (.csvs) as rasters, 
-# to serve as canonical inputs going forward.
-# This is adapted from dt_age_to_raster.R
+# 3. Pass temporal filter across land cover maps ----
+# Adapted from "temporal_filter_lc.R"
 
 # load the data
-tic("load data")
-dt <- fread(file = paste0(p_input_rasters, site, ".csv"))
+tic("load lc data.table")
+lc_dt <- fread(file = paste0(p_input_rasters, site, ".csv"))
 toc(log = TRUE)
 
-# convert age dt to raster
-tic("convert dt to raster")
-r <- dt_to_raster(dt, crs("+proj=longlat +datum=WGS84 +no_defs"))
+print(paste0("ncell, before filter: ", nrow(lc_dt)))
+
+# pass temporal filter
+tic("pass temporal filter")
+cc_temporal_filter_lc(lc_dt)
+toc()
+
+print(paste0("ncell, after filter: ", nrow(lc_dt)))
+
+# write to file
+fwrite(lc_dt, file = paste0(p_input_rasters, site, "_clean.csv"))
+
+cat(paste0("Success! Passed temporal filter on land cover data.table for site: ", site_df$site[indx]), fill = TRUE)
+
+
+# -------------------------------------------------------- #
+# 4. Save renamed and recoded input land cover data.tables (.csvs) as rasters ----
+# to serve as canonical inputs going forward.
+# This is adapted from "dt_age_to_raster.R"
+
+# convert dts to raster
+# read back in the un-filtered version:
+lc_dt_unfiltered <- fread(file = paste0(p_input_rasters, site, ".csv"))
+
+tic("convert un-filtered dt to raster")
+r_unfiltered <- dt_to_raster(lc_dt_unfiltered, crs("+proj=longlat +datum=WGS84 +no_defs"))
+toc(log = TRUE)
+
+tic("convert cleaned dt to raster")
+r <- dt_to_raster(lc_dt, crs("+proj=longlat +datum=WGS84 +no_defs"))
 toc(log = TRUE)
 
 names(r)
 print(r)
 
-
 # write raster
-tic("write raster")
-writeRaster(r, filename = paste0(p_input_rasters, site, ".tif"))
+tic("write un-filtered raster")
+raster::writeRaster(r_unfiltered, 
+                    filename = paste0(p_input_rasters, site, "_tmp.tif"),
+                    overwrite = TRUE)
 toc(log = TRUE)
 
+tic("write cleaned raster")
+raster::writeRaster(r, 
+                    filename = paste0(p_input_rasters, site, "_clean_tmp.tif"),
+                    overwrite = TRUE)
+toc(log = TRUE)
+
+# reload raster as SpatRaster
+r_unfiltered <- terra::rast(paste0(p_input_rasters, site, "_tmp.tif"))
+r <- terra::rast(paste0(p_input_rasters, site, "_clean_tmp.tif"))
+
+# extract names:
+# rename raster layers:
+if (site == "nebraska") {
+  layer_names <- paste0("y", 1986:2018)
+} else {
+  if (site == "wisconsin") {
+    layer_names <- paste0("y", 1987:2018)
+  } else {
+    # everything else, just 1987:2017
+    layer_names <- paste0("y", 1987:2017)
+  }}
+
+# write new cleaned SpatRasters to file, with layer names
+tic("write new cleaned lc raster to file")
+terra::writeRaster(r,
+                   filename = paste0(p_input_rasters, site, "_clean.tif"),
+                   overwrite = TRUE,
+                   names = layer_names)
+toc()
+
+tic("write unfiltered lc raster to file")
+terra::writeRaster(r_unfiltered,
+                   filename = paste0(p_input_rasters, site, ".tif"),
+                   overwrite = TRUE,
+                   names = layer_names)
+toc()
 
 # -------------------------------------------------------- #
 
